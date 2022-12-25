9aed1fab78bf959bdbefd20de4a437d5061e8f25
jh <zongjianhui@hualala.com>
2022-12-25 18:50:49 +0800

    init

---
+++ src/main/java/com/google/grpc/learning/helloworld/client/HelloWorldClient.java
@@ -0,0 +1,81 @@
+package com.google.grpc.learning.helloworld.client;
+
+import com.google.grpc.learning.helloworld.GreeterGrpc;
+import com.google.grpc.learning.helloworld.HelloReply;
+import com.google.grpc.learning.helloworld.HelloRequest;
+import com.google.grpc.learning.helloworld.server.HelloWorldServer;
+import io.grpc.Channel;
+import io.grpc.Grpc;
+import io.grpc.InsecureChannelCredentials;
+import io.grpc.ManagedChannel;
+import io.grpc.StatusRuntimeException;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * A simple client that requests a greeting from the {@link HelloWorldServer}.
+ */
+public class HelloWorldClient {
+
+    private final GreeterGrpc.GreeterBlockingStub blockingStub;
+
+    /** Construct client for accessing HelloWorld server using the existing channel. */
+    public HelloWorldClient(Channel channel) {
+        // 'channel' here is a Channel, not a ManagedChannel, so it is not this code's responsibility to
+        // shut it down.
+
+        // Passing Channels to code makes code easier to test and makes it easier to reuse Channels.
+        blockingStub = GreeterGrpc.newBlockingStub(channel);
+    }
+
+    /** Say hello to server. */
+    public void greet(String name) {
+        System.out.println("Will try to greet " + name + " ...");
+        HelloRequest request = HelloRequest.newBuilder().setName(name).build();
+        HelloReply response;
+        try {
+            response = blockingStub.sayHello(request);
+        } catch (StatusRuntimeException e) {
+            System.out.println( "RPC failed: "+ e.getStatus());
+            return;
+        }
+        System.out.println("Greeting: " + response.getMessage());
+    }
+
+    /**
+     * Greet server. If provided, the first element of {@code args} is the name to use in the
+     * greeting. The second argument is the target server.
+     */
+    public static void main(String[] args) throws Exception {
+        String user = "world";
+        // Access a service running on the local machine on port 50051
+        String target = "localhost:50051";
+        // Allow passing in the user and target strings as command line arguments
+        if (args.length > 0) {
+            if ("--help".equals(args[0])) {
+                System.err.println("Usage: [name [target]]");
+                System.err.println("");
+                System.err.println("  name    The name you wish to be greeted by. Defaults to " + user);
+                System.err.println("  target  The server to connect to. Defaults to " + target);
+                System.exit(1);
+            }
+            user = args[0];
+        }
+        if (args.length > 1) {
+            target = args[1];
+        }
+
+
+        ManagedChannel channel = Grpc.newChannelBuilder(target, InsecureChannelCredentials.create())
+                .build();
+        try {
+            HelloWorldClient client = new HelloWorldClient(channel);
+            client.greet(user);
+        } finally {
+            // ManagedChannels use resources like threads and TCP connections. To prevent leaking these
+            // resources the channel should be shut down when it will no longer be used. If it may be used
+            // again leave it running.
+            channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
+        }
+    }
+}
